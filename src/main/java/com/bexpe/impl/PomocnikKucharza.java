6ac97d407446e9b6b003c68a0717cc1cbec8b26f
bexpe <beata.k.puzio@gmail.com>
2019-08-29 10:12:12 +0200

    Create a new class

---
+++ src/main/java/com/bexpe/impl/PomocnikKucharza.java
@@ -0,0 +1,34 @@
+package com.bexpe.impl;
+
+import com.bexpe.api.IKucharz;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class PomocnikKucharza implements IKucharz {
+
+    @Autowired
+    @Qualifier("pomocnik kucharza")
+    private IKucharz kucharz;
+
+    public void pobierzSkadnikizMagazynu(int liczbaSkładnikow) {
+        System.out.println("Pomocnik pobiera " + liczbaSkładnikow + "z Magazynu");
+    }
+
+    public void pobierzSkladnikiZMagazynu() {
+        System.out.println("Pomocnik pobiera skladniki z magazynu");
+    }
+
+    public void przygotujPizze() {
+        System.out.println("Pomocnik oddelogowuje przygotowanie do kucharza");
+        kucharz.przygotujPizze();
+    }
+
+    public void przygotujPizze(int idPizzy) {
+        System.out.println("Pomocnik oddelogowuje przygotowanie pizzy o id " + idPizzy + "do kucharza");
+        kucharz.przygotujPizze(idPizzy);
+    }
+
+    public void wydajPizze() {
+        System.out.println("Pomocnik wydaje pizze");
+    }
+}
