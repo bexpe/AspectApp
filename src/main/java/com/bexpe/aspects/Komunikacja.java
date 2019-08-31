fce616d608619080d11c2adf8c76f425baa78ecc
bexpe <beata.k.puzio@gmail.com>
2019-08-31 12:58:21 +0200

    Add new aspect

--- src/main/java/com/bexpe/aspects/Komunikacja.java
+++ src/main/java/com/bexpe/aspects/Komunikacja.java
@@ -1,28 +1,45 @@
 package com.bexpe.aspects;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.*;
 
 @Aspect
 public class Komunikacja {
 
-    @After("execution(* com.bexpe.api.IKucharz.pobierzSkladnikiZMagazynu(..))")
-    public void poinformujOPobraniuSkladnikow() {
-        System.out.println("pobrano skladniki");
+    @Pointcut("args(argumentInt)")
+    public void pointcutInt(int argumentInt) {}
+
+    @Pointcut("target(com.bexpe.api.IKucharz)")
+    public void pointcutTarget(){
     }
 
-    @Around("execution(* com.bexpe.api.IKucharz.wydajPizze(..))")
-    public void wydaniePizzy(ProceedingJoinPoint joinPoint) {
-        try {
-            System.out.println("Pizza jest gotowa do wydania");
-            joinPoint.proceed();
-            System.out.println("Wydano pizze");
-
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
+    @Pointcut("@target(annotation)")
+    public void pointcutTargetAnnotation(Deprecated annotation){
     }
+
+    @Pointcut("within(com.bexpe.impl.Kucharz)")
+    public void pointcutWithin(){
+    }
+
+    @Before("pointcutWithin()")
+    public void porada() {
+        System.out.println("PORADA ");
+    }
+
+//    @After("execution(* com.bexpe.api.IKucharz.pobierzSkladnikiZMagazynu(..))")
+//    public void poinformujOPobraniuSkladnikow() {
+//        System.out.println("pobrano skladniki");
+//    }
+//
+//    @Around("execution(* com.bexpe.api.IKucharz.wydajPizze(..))")
+//    public void wydaniePizzy(ProceedingJoinPoint joinPoint) {
+//        try {
+//            System.out.println("Pizza jest gotowa do wydania");
+//            joinPoint.proceed();
+//            System.out.println("Wydano pizze");
+//
+//        } catch (Throwable e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
