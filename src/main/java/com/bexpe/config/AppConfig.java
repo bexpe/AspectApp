13ebc03655c66dcb57cefa7d5bd30cef72b30930
bexpe <beata.k.puzio@gmail.com>
2019-09-08 14:47:11 +0200

    Add hibernate configuration

--- src/main/java/com/bexpe/config/AppConfig.java
+++ src/main/java/com/bexpe/config/AppConfig.java
@@ -8,10 +8,17 @@ import com.bexpe.aspects.WarehouseControl;
 import com.bexpe.impl.CasseroleImpl;
 import com.bexpe.impl.Cook;
 import com.bexpe.impl.CooksHelper;
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableAspectJAutoProxy //niezbedna adnotacja przy aspektach musimy o niej pamietac
@@ -47,5 +54,45 @@ public class AppConfig {
     @Bean
     public WarehouseControl warehouseControl() {
         return new WarehouseControl();
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(
+                "com.baeldung.hibernate.bootstrap.model");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("sa");
+
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager
+                = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+
+    private final Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty(
+                "hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty(
+                "hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+
+        return hibernateProperties;
     }
 }
