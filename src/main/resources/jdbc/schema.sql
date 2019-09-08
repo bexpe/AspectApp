90513900dcca89d7e9690845979172727b7d812b
bexpe <beata.k.puzio@gmail.com>
2019-09-09 00:20:18 +0200

    Add sample db sql

---
+++ src/main/resources/jdbc/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE COOK
+
+(
+    ID int NOT NULL PRIMARY KEY,
+    NAME varchar(255),
+    SURNAME varchar(255),
+    AGE int,
+);
---
+++ src/main/resources/jdbc/test-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO COOK VALUES (1, 'James', 'Bonds', 34);
+
+INSERT INTO COOK VALUES (2, 'Donald', 'Truth', 25);
+
+INSERT INTO COOK VALUES (3, 'Lisa', 'Jones', 28);
+
+INSERT INTO COOK VALUES (4, 'Kelly', 'Ritchie', 40);
